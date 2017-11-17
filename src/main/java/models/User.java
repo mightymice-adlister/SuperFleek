6d89a1fd4f17f1c0c765a1b4249aef4f24314de3
Uglygrayduck <Nicholasgswan@gmail.com>
2017-11-17 11:32:24 -0600

    Create all models add getters and setters for all models add constructors for some

---
+++ src/main/java/models/User.java
@@ -0,0 +1,79 @@
+package models;
+
+import java.util.List;
+
+public class User {
+    private Long id;
+    private String email;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private List<MakeupList> makeupLists;
+    private List<Look> lookList;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<MakeupList> getMakeupLists() {
+        return makeupLists;
+    }
+
+    public void setMakeupLists(List<MakeupList> makeupLists) {
+        this.makeupLists = makeupLists;
+    }
+
+    public List<Look> getLookList() {
+        return lookList;
+    }
+
+    public void setLookList(List<Look> lookList) {
+        this.lookList = lookList;
+    }
+}
