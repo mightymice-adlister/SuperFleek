26d5b9793f5d9e6f1622660a5254037999d1d85c
Uglygrayduck <Nicholasgswan@gmail.com>
2017-11-17 09:48:38 -0600

    Initial commit

---
+++ src/main/java/com/mightymice/superfleek/SuperfleekApplication.java
@@ -0,0 +1,12 @@
+package com.mightymice.superfleek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperfleekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperfleekApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mightymice/superfleek/SuperfleekApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mightymice.superfleek;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SuperfleekApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
