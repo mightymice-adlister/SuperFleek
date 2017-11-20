4fa1b796f0327aacf5dee00bbc964a0b89d0c5ff
Jordan Leslie <this.codedev@gmail.com>
2017-11-20 10:03:02 -0600

    Add missing curly brace in makeup.java

--- src/main/java/com/mightymice/superfleek/models/Makeup.java
+++ src/main/java/com/mightymice/superfleek/models/Makeup.java
@@ -6,31 +6,33 @@ import java.util.List;
 @Entity
 @Table(name="makeup")
 public class Makeup {
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     private Long id;
     @Column(nullable = false)
     private String name;
     @ManyToMany
     @JoinTable(
-            name="makeup_categories",
-            joinColumns = {@JoinColumn(name="makeup_id")},
-            inverseJoinColumns = {@JoinColumn(name="category_id")}
+            name = "makeup_categories",
+            joinColumns = {@JoinColumn(name = "makeup_id")},
+            inverseJoinColumns = {@JoinColumn(name = "category_id")}
     )
     private List<Category> categories;
     @ManyToOne
-    @JoinColumn(name="brand_id")
+    @JoinColumn(name = "brand_id")
     private Brand brand;
     @Column
     private String colorName;
     @ManyToOne
-    @JoinColumn(name="color_family_id")
+    @JoinColumn(name = "color_family_id")
     private ColorFamily colorFamily;
     @Column
     private String finish;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy="makeup" )
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "makeup")
     private List<Review> reviews;
     @ManyToMany(mappedBy = "makeups")
     private List<Look> looks;
     @ManyToMany(mappedBy = "makeups")
     private List<MakeupList> makeupLists;
 
+}
