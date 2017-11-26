f8337581b53e47ec622cb39f8240b5d728aa4f69
Uglygrayduck <Nicholasgswan@gmail.com>
2017-11-26 14:48:57 -0600

    Added some things to models, created new interface -Added getters and setters to those models/properties without them -Added default constructors where there were none -Created Looks interface -Created method in Looks interface to get profile picture -Created method to get Looks by User

--- src/main/java/com/mightymice/superfleek/models/MakeupType.java
+++ src/main/java/com/mightymice/superfleek/models/MakeupType.java
@@ -13,4 +13,31 @@ public class MakeupType {
     private String name;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "type")
     private List<Makeup> makeupsWithType;
+
+    public MakeupType() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Makeup> getMakeupsWithType() {
+        return makeupsWithType;
+    }
+
+    public void setMakeupsWithType(List<Makeup> makeupsWithType) {
+        this.makeupsWithType = makeupsWithType;
+    }
 }
