d03346734306cf292e45e83f0dea07fb91514652
Jordan Leslie <this.codedev@gmail.com>
2017-11-26 12:08:48 -0600

    Add filestack api to profile pic upload button on make-profile view

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -2,4 +2,24 @@ $(document).ready(function() {
   // Navbar collapse on med screens and smaller
   $(".button-collapse").sideNav();
   console.log("hello");
+
+  var fsClient = filestack.init('ArukrJULXTZWDKklrkBmaz');
+  function openPicker() {
+    fsClient.pick({
+      fromSources:["local_file_system","imagesearch","facebook","instagram","webcam"],
+      accept:["image/*"],
+      maxFiles:1,
+      transformations:{
+        crop:{      force:true}}
+    }).then(function(response) {
+      // declare this function to handle response
+      function handleFilestack(response) {
+        console.log(response);
+      }
+      handleFilestack(response);
+    });
+  }
+
+  $(".add-profile-pic").on("click", openPicker);
+
 });
